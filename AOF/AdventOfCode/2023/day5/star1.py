cc181aece3727e18fd9dea1642f30998c7426f40
OussamaAnadaniCGI <131246979+OussamaAnadaniCGI@users.noreply.github.com>
2024-11-13 10:13:40 +0100

    day5 part1 almost done

--- AOF/AdventOfCode/2023/day5/star1.py
+++ AOF/AdventOfCode/2023/day5/star1.py
@@ -1,4 +1,3 @@
-
 def getData():
     with open('C:\\Users\\oussama.anadani\\Desktop\\PythonOnlineProblems\\AOF\\AdventOfCode\\2023\\day5\\data.txt') as f:
         data = f.read()
@@ -7,34 +6,47 @@ def getData():
 def data_preProcessing(data):
     data = data.strip().split("\n\n")
     seeds = data[0].split(":")[1].split()
-    all_maps = [data[idx].split("map:") for idx in range(1,len(data))]
-    return seeds, all_maps
+    categories = [data[idx].split("map:") for idx in range(1,len(data))]
+    return seeds, categories
 
-def create_mapList(dist,src,range_):
-    src_list = []
-    dist_list = []
-    src_list.append(int(src))
-    dist_list.append(int(dist))
-    for _ in range(0,int(range_)-1):
-        src = int(src) + 1
-        dist = int(dist)+ 1
-        src_list.append(src)
-        dist_list.append(dist)
-    return src_list, dist_list
-
-def split_maps(maps):
-    for map in maps:
-        map_name, map_values = map
-        lines = map_values.strip().splitlines()
-        for line in lines:
-            dist, src, range_= line.split()
-            src, dist = create_mapList(dist,src,range_)
+def check_for_src_in_range(seed, category_lines):
+    for line in category_lines: 
+        dist, src, length= line.split()
+        src, dist, length = int(src),int(dist),int(length)
+        if seed >= src and seed <= (src+length):
+            return dist, src, length
 
 
-def star1():
+def ceate_conversion_map(current_src, category_lines):
+    src_dist_map={}
+    result = check_for_src_in_range(current_src, category_lines)         
+    if result is not None:
+        dist, src, length = result
+        for _ in range(length):
+            src_dist_map[src]=dist
+            src +=1
+            dist +=1
+    return src_dist_map
+
+def search_in_map(seed, src_dist_map):
+    return src_dist_map.get(seed,seed)
+
+def get_min_location(seed, categories):
+    current_src = seed
+    for ctgry_name, category_lines in categories:    
+        category_lines = category_lines.strip().splitlines()
+        src_dist_map = ceate_conversion_map(current_src, category_lines)
+        if src_dist_map:
+            distination=search_in_map(current_src, src_dist_map)
+            current_src=distination
+    return current_src
+
+
+def star1():    
     data=getData()
-    seeds,all_maps=data_preProcessing(data)
-    maps= split_maps(all_maps)
+    seeds, categories=data_preProcessing(data)
+    all_locaions=[get_min_location(int(seed), categories) for seed in seeds]    
+    print(min(all_locaions))
 
 if __name__ == "__main__":
     star1()
