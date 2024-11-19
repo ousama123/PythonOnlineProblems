4349107138b05547a65fa78835ce2b19e310a81a
OussamaAnadaniCGI <131246979+OussamaAnadaniCGI@users.noreply.github.com>
2024-11-19 16:32:45 +0100

    2021 day4 first version

--- AOF/AdventOfCode/2021/day4/day42021.py
+++ AOF/AdventOfCode/2021/day4/day42021.py
@@ -1,35 +1,42 @@
+class Value:
+    def __init__(self, value: int, position: tuple):
+        self.marked= False
+        self.value = value
+        self.position= position
 
-from collections import defaultdict
-import numpy as np
-data=defaultdict()
-marked = False
-matrix=[]
-final_matrix = []
-with open("data4.txt") as f:
-    lines = f.readlines()
-    for i in range(0,len(lines)):
-        #lines[i] = int(str(lines[i]))
-        if i==0:
-            values=lines[i].split(',')
-            for j in values:
-                data[int(j)] = marked
-
-        if lines[i] != "\n" and i != 0:
-            #a new matrix approached
-            line = (lines[i].split())
-            matrix.append(line)
-
-    print("bo")
-    print(matrix)    
+class Board:
+    def __init__(self, board, idx):
+        self.idx=idx
+        self.parsed_board = self.parse_board(board)
     
-            
-
-
-
-    
+    def parse_board(self, board):
+        parsed_board=[]
+        for row_idx, row in enumerate(board.splitlines()):
+            for col_idx, col in enumerate(row.split()):
+                col=int(col)
+                parsed_board.append(Value(col, (row_idx,col_idx)))
         
+        return parsed_board
 
-        
-            
+
+def get_data():
+    with open(r'C:\Users\oussama.anadani\Desktop\PythonOnlineProblems\AOF\AdventOfCode\2021\day4\data.txt') as f:
+        return f.read()
     
+def main():
+    data=get_data()
+    numbers, *boards=data.split('\n\n')
+    numbers=list(map(int,numbers.split(",")))
     
+    fixed_boards=[Board(board, idx) for idx, board in enumerate(boards)]
+
+    for number in numbers:
+        for board in fixed_boards:
+            for element in board.parsed_board:
+                if number == element.value:
+                    element.marked= True
+    
+    print([ele.marked for ele in board.parsed_board])
+
+if __name__=="__main__":
+    main()
