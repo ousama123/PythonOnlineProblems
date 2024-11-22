2e82ad4b1e1b5da143b2064d391cc6b888aaeb1f
OussamaAnadaniCGI <131246979+OussamaAnadaniCGI@users.noreply.github.com>
2024-11-22 12:30:41 +0100

    day 3 part1 is done 2020

---
+++ AOF/AdventOfCode/2020/day3/day3.py
@@ -0,0 +1,31 @@
+def get_data():
+    with open('C:\\Users\\oussama.anadani\\Desktop\\PythonOnlineProblems\\AOF\\AdventOfCode\\2020\\day3\\data.txt') as f:
+        return f.read()
+
+def count_trees(grid):
+    trs_count=0
+    col_idx = 3
+    for r_idx, row in enumerate(grid.splitlines()):
+        if(r_idx == 0):
+            continue
+        
+        value = row[col_idx]
+        if value == '#':
+            trs_count +=1
+        
+        col_idx +=3
+
+        if col_idx >= len(row):
+            col_idx -= len(row)
+            
+    return trs_count
+
+def main():
+    data = get_data()
+    #print(data)
+    trs_count=count_trees(data)
+    print(trs_count)
+
+   
+if __name__ == "__main__":
+    main()
