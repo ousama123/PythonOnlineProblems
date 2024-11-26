f87d997cc524f20718777c804ba7229cea767aec
OussamaAnadaniCGI <131246979+OussamaAnadaniCGI@users.noreply.github.com>
2024-11-26 15:58:15 +0100

    part2 2021 alsmost done

--- AOF/AdventOfCode/2021/day7/day7.py
+++ AOF/AdventOfCode/2021/day7/day7.py
@@ -14,43 +14,37 @@ def part1(positions):
     pos_sum=0
     fuel_sums=[]
     for pos in positions:
-        pos_sum+= abs(statistics.median(positions) - pos)
+        median_value=statistics.median(positions)
+        pos_sum+= abs(median_value - pos)
     fuel_sums.append(pos_sum)
 
     return min(fuel_sums)
 
 def part2(positions):
-    step = 0 
-    sum_pos=0
-    sum_list = []
-    for pos in positions:
-        sum_pos=0
-        for pos2 in positions:
-            step=0
-            if pos > pos2:
-                for fuel in range(pos2, pos):        
-                    step +=1
-            if pos<pos2:
-                for fuel in range(pos,pos2):
-                    step +=1
-            if pos == pos2:
-                step +=0
-            
-            print(f"Move from {pos2} to {pos}: ", step, " fuel")
-            sum_pos+=step
-        sum_list.append(sum_pos)
-        print(f"Sum for position {pos} is: " , sum_pos)
-        print()
-    print(min(sum_list))
-            
+    mean_value = statistics.mean(positions)
 
+    lower_mean, higher_mean = positions[0], positions[-1]
+    all_fuel_costs = 0
+    mean_values= [mean_value, lower_mean, higher_mean]
+    all_fule_costs_with_different_means = []
+    for mean_val in range():
+        for pos in positions:
+            dist=abs(mean_val - pos)
+            fuel_cost = (dist * (dist + 1)) // 2
+            all_fuel_costs += fuel_cost
+        
+        all_fule_costs_with_different_means.append((all_fuel_costs))
+
+    print((all_fule_costs_with_different_means))
+            
+            
 def main():
     data=get_data()
     positions=list(map(int,data.split(',')))
     positions = sorted(positions)
     fuel_sums=part1(positions)
-    print(int(fuel_sums))
-    #part2(positions)
+    #print(int(fuel_sums))
+    part2(positions)
    
 if __name__ == "__main__":
     main()
