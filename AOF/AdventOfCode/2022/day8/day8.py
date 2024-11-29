c51f09d48b76e4758b641124b6e08e1ee0aa9545
OussamaAnadaniCGI <131246979+OussamaAnadaniCGI@users.noreply.github.com>
2024-11-29 15:14:34 +0100

    2022 day8 part1 done

--- AOF/AdventOfCode/2022/day8/day8.py
+++ AOF/AdventOfCode/2022/day8/day8.py
@@ -1,26 +1,38 @@
+from dotenv import load_dotenv
+import os
 
-def main(visible,current):
-    with open("data8.txt") as f:
-        lines = f.readlines()
-        for i in range(len(lines)):
-            for j in range(len(lines)):
-                if i == 0 or j == 0 or i == len(lines)-1 or j == len(lines)-1: #counting the edges
-                    visible +=1
-                current = lines[i][j]
-                visible =  check_same_row_col(current,i,j,lines,visible)
-        return visible
+load_dotenv()
+FILE_PATH = os.getenv('FILE_PATH')
+FILE_PATH_DEBUG = os.getenv('FILE_PATH_DEBUG')
 
-def check_same_row_col(current,i,j,lines,visible):
-    for k in range(len(lines)):
-        if current < lines[i][k] and current < lines[k][j]:
-            visible +=1
-    return visible
+def get_data():
+    with open(FILE_PATH_DEBUG) as f:
+        return f.read()
+    
 
-if __name__=="main":
-    visible = 0
-    current = 0
-    visible=main(visible)
-    print(visible)
+def part1(grid):
+    visible_trees = 0
+    for row_idx in range(0,len(grid)):
+        for col_idx in range(0,len(grid[0])):
+            current = (grid[row_idx][col_idx])
+            if (
+                all(grid[row_idx][left_idx] < current for left_idx in range(col_idx ))
+                or all(grid[row_idx][right_idx] < current for right_idx in range(col_idx + 1, len(grid[0])))
+                or all(grid[upper_idx][col_idx] < current for upper_idx in range(row_idx ))
+                or all(grid[lower_idx][col_idx] < current for lower_idx in range(row_idx + 1, len(grid)))
+            ):
+                visible_trees +=1
+    print(visible_trees)
+                
+                            
+def main():
+    data=get_data()
+    grid = [list(map(int, line)) for line in data.splitlines()]
+    part1(grid)
+    
+
+if __name__ == "__main__":
+    main()
             
 
 
