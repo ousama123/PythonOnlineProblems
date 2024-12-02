a3660d947ca928e59ca0034ce15a7cd2dd8dfc96
OussamaAnadaniCGI <131246979+OussamaAnadaniCGI@users.noreply.github.com>
2024-12-02 16:17:21 +0100

    2024 day2 is done

--- AOF/AdventOfCode/2024/day2/day2.py
+++ AOF/AdventOfCode/2024/day2/day2.py
@@ -21,6 +21,14 @@ def ckeck_decreasing(report):
             return False
     return True
 
+def check_safe_report(report):
+    safe_report= False
+    if report[0] < report[1]:
+        safe_report=ckeck_increasing(report)
+    elif report[0] > report[1]:
+        safe_report=ckeck_decreasing(report)
+    
+    return safe_report
 
 def part1(data):
     safe_report=False
@@ -29,18 +37,35 @@ def part1(data):
         safe_report=False
         report = report.split()
         report=list(map(int,report))
-        if report[0] < report[1]:
-            safe_report=ckeck_increasing(report)
-        elif report[0] > report[1]:
-            safe_report=ckeck_decreasing(report)
-
+        safe_report=check_safe_report(report)
         if(safe_report):
             safe_reports+=1
     
     print(safe_reports)
 
 def part2(data):
-    pass
+    safe_report=False
+    safe_reports=0
+    for report in data.splitlines():
+        safe_report=False
+        report = report.split()
+        report=list(map(int,report))
+
+        safe_report= check_safe_report(report)
+        
+        if not safe_report:
+            for idx in range(len(report)):
+                new_report=report[:idx] + report[idx+1:]
+                safe_report=check_safe_report(new_report)
+                
+                #break the loop once you get at least one safe report
+                if(safe_report):                    
+                    break
+        
+        if safe_report:
+            safe_reports +=1
+    
+    print(safe_reports)
 
 def main():
     data=get_data()
